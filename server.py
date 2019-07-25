1fe2d5b77ffdb0d89a90fd36f67ba840094f82d1
Timothy <me@timothylock.ca>
2019-07-25 10:22:09 -0400

    Support env vars

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 from flask import Flask, request
-from flask import send_from_directory
-import os
 from twilio.twiml.voice_response import VoiceResponse
 import sqlite3
 import datetime
+import os
 
 app = Flask(__name__)
 
@@ -12,32 +11,40 @@ db = sqlite3.connect('storage.db')
 c = db.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS events(validuntil INTEGER)")
 
+# Fetch env vars
+whitelisted_numbers = os.environ['WHITELISTED_NUMBERS'].split(",")          # Numbers allowed to dial into the system
+buzzcode = os.environ['BUZZCODE']                                           # Digits to dial to let them in
+minutes = int(os.environ['minutes'])                                        # Number of minutes to unlock the system
+
+
 # Buzzer
 ##########################################################################
 @app.route("/buzzer/webhook", methods=['GET', 'POST'])
 def voice():
     """Respond to incoming phone calls"""
     resp = VoiceResponse()
+    incoming_number = request.values['From']
 
     # Reject call if the originating number is not from the intercom system
     # Does not incur any cost on twillio but is a shitty user experience
-    if False:
+    if incoming_number not in whitelisted_numbers:
         resp.reject("not allowed to buzz")
 
     # Tell the user a nice message that they are not permitted to enter
-    if not allowedToBuzz():
+    if not allowed_to_buzz():
         resp.say("The system cannot let you in. Please tell the host to click allow on their phone")
         return str(resp)
 
     # Otherwise, unlock the door
     resp.say("unlocking door. Please wait.")
     resp.play(digits='6666')
-    resp.say("code injected. If you still hear this, please contact whoever you are trying to reach as the unlock has failed. Goodbye")
+    resp.say("code injected. If you still hear this, please contact whoever you are trying to reach manually. Goodbye")
 
     return str(resp)
 
+
 @app.route("/buzzer/status/allow", methods=['GET'])
-def statusAllow():
+def status_allow():
     """Tells the buzzer to unlock the door for the next 30 minutes"""
     expire = datetime.datetime.now() + datetime.timedelta(minutes=30)
 
@@ -48,12 +55,14 @@ def statusAllow():
 
     return str("Success. Will buzz everybody in until " + expire.strftime("%m/%d/%Y, %H:%M:%S"))
 
+
 @app.route("/buzzer/status/", methods=['GET'])
 def status():
     """Fetches whether the system will buzz people in"""
-    return str(allowedToBuzz())
+    return str(allowed_to_buzz())
 
-def allowedToBuzz():
+
+def allowed_to_buzz():
     """Fetches whether the system is allowed to buzz somebody in"""
     now = datetime.datetime.now()
 
@@ -63,6 +72,7 @@ def allowedToBuzz():
     db.commit()
 
     return c.fetchone()[0] >= 1
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
